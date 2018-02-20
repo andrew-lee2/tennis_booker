a97608495575e3b5c680cd2678c9de237e08240e
andrewlee <andrewlee@indeed.com>
2018-02-19 19:39:02 -0600

    trying more stuff

--- test.py
+++ test.py
@@ -1,23 +1,32 @@
-from datetime import datetime
-import time
+"""
+This example demonstrates the use of the SQLAlchemy job store.
+On each run, it adds a new alarm that fires after ten seconds.
+You can exit the program, restart it and observe that any previous alarms that have not fired yet
+are still active. You can also give it the database URL as an argument.
+See the SQLAlchemy documentation on how to construct those.
+"""
+
+from datetime import datetime, timedelta
+import sys
 import os
-from apscheduler.schedulers.background import BackgroundScheduler
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def tick():
-    print('Tick! The time is: %s' % datetime.now())
+def alarm(time):
+    print('Alarm! This alarm was scheduled at %s.' % time)
 
 
 if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds=3)
-    scheduler.start()
+    scheduler = BlockingScheduler()
+    url = sys.argv[1] if len(sys.argv) > 1 else 'sqlite:///example.sqlite'
+    scheduler.add_jobstore('sqlalchemy', url=url)
+    alarm_time = datetime.now() + timedelta(seconds=10)
+    scheduler.add_job(alarm, 'date', run_date=alarm_time, args=[datetime.now()])
+    print('To clear the alarms, delete the example.sqlite file.')
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
-        # This is here to simulate application activity (which keeps the main thread alive).
-        while True:
-            time.sleep(2)
+        scheduler.start()
     except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
+        pass
